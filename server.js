fbf43d3a04a7cd593ba0c93f3ece3bae8cfbce1e
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-03-23 10:39:58 +0100

    started creating bootcamps routes

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+// route files
+const bootcamps = require('./routes/bootcamps.routes')
+
 // load env vars
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
+
+// mount routers
+app.use('/api/v1/bootcamps', bootcamps)
+
+app.get('/', (req, res) => {
+	res.json({ text: 'Hello from Express, mofo' })
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(
